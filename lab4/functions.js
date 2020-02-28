ef9ec480b592765dc7c2698bae6817a52eb88fe1
unknown <yb.26.code@gmail.com>
2020-02-27 22:57:56 -0500

    Updated select options with js

--- lab4/functions.js
+++ lab4/functions.js
@@ -48,3 +48,54 @@ function makeInput(type,id,value,label) {
 	
 	return newDiv;
 }
+
+
+function makeSelect(id,value,label) {
+	
+	
+	var newSelect = document.createElement("select");
+	var newDiv = document.createElement("div");;
+	var  newLabel; 
+	
+	
+	if (id !== undefined && id != "") {
+		newSelect.setAttribute("id", id);
+	}
+	
+	//if (value!== undefined && value != "") {
+	//	newSelect.setAttribute("name", value);
+	//}
+	
+	
+	for (var i = 0; i < value.length;i++ ){
+		var makeOption = document.createElement("option");
+	if (value[i]!== undefined && value[i] != "") {
+		makeOption.setAttribute("value", value[i]);
+	}
+	
+	   makeOption.appendChild(makeTextNode(value[i]));
+	   newSelect.appendChild(makeOption);
+	}
+	
+	if (label!== undefined && label != "") {
+		newLabel= makeLabel(label, id);
+		newDiv.appendChild(newLabel);
+		
+		//newDiv = document.createElement("div");
+		//newDiv.appendChild(newLabel);
+		//newDiv.appendChild(newSelect);
+		
+	}
+	
+	
+	//var  newLabel= makeLabel(label, id);
+	newDiv.appendChild(newSelect);
+	
+	
+	
+	
+	return newDiv;
+}
+
+
+
